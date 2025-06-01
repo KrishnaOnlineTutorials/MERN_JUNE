565132360a03182687d5f701640dfc439b8de047
KrishnaOnlineTutorials <krishnaonlinetutorials@gmail.com>
2025-06-01 10:44:53 +0530

    Adding a new component HelloWorld to the frontend

---
+++ frontend/src/components/HelloWorld.js
@@ -0,0 +1,10 @@
+const HelloWorld = () => {
+    return (
+        <div>
+        <h1>Hello World!</h1>
+        <p>This is a simple React component.</p>
+        </div>
+    );
+}
+
+export default HelloWorld;
