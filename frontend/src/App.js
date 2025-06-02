94b108fcb948090abbae61b2227da97edad6d0f7
KrishnaOnlineTutorials <krishnaonlinetutorials@gmail.com>
2025-06-02 10:52:01 +0530

    Talking about Component, props, loops, object destructuring and spread operater

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,23 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 
 function App() {
+  const users = [
+    { userName: 'userOne', age: 25, location: 'New York', contact: '123456' },
+    { userName: 'userTwo', age: 30, location: 'Los Angeles', contact: '654321' },
+    { userName: 'userThree', age: 28, location: 'Chicago', contact: '789012' },
+    { userName: 'userFour', age: 35, location: 'Houston', contact: '345678' },
+    { userName: 'userFive', age: 20, location: 'Phoenix', contact: '901234' },
+    { userName: 'userSix', age: 20, location: 'Philadelphia', contact: '567890' },
+    { userName: 'userSeven', age: 30, location: 'San Antonio', contact: '234567' },
+    { userName: 'userEight', age: 22, location: 'San Diego', contact: '890123' },
+    { userName: 'userNine', age: 27, location: 'Dallas', contact: '456789' },
+    { userName: 'userTen', age: 33, location: 'San Jose', contact: '123789' }
+  ];
   return (
     <div className="App">
-      <HelloWorld />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user, index) => 
+        <HelloWorld key={index} {...user} />
+      )}
     </div>
   );
 }
--- frontend/src/components/HelloWorld.js
+++ frontend/src/components/HelloWorld.js
@@ -1,8 +1,14 @@
-const HelloWorld = () => {
+const HelloWorld = (props) => {// props is an object
+    const {userName, age, location, contact} = props; // destructuring assignment
+    
     return (
         <div>
-        <h1>Hello World!</h1>
-        <p>This is a simple React component.</p>
+            <h1>{userName}</h1> 
+            <h2>{age}</h2>
+            <h3>{location}</h3>
+            <h4>{contact}</h4>
+
+            <p>Welcome to the world of React!</p>
         </div>
     );
 }
